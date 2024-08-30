166235acc581baf69928b1896e0f534304b60fc6
samb1232 <arterm212@gmail.com>
2024-08-30 23:49:49 +0500

    changed subs generation to json

--- external_modules/sub_parser.py
+++ external_modules/sub_parser.py
@@ -1,22 +1,165 @@
+import json
 import re
 from datetime import datetime, timedelta
 
 
 class Subtitle:
-    def __init__(self, number, start_time, end_time, duration, text):
-        self.number = number
+    def __init__(self, id: int, start_time: int, end_time: int, text: str, speaker: str | None = None):
+        self.id = id
         self.start_time = start_time
         self.end_time = end_time
-        self.duration = duration
+        self.duration = self.start_time - self.end_time
+        self.speaker = speaker
         self.text = text
 
     def __repr__(self):
-        return f"Subtitle(number={self.number}, start_time={self.start_time}, end_time={self.end_time}, duration={self.duration}, text={self.text})"
+        return f"Subtitle(number={self.id}, start_time={self.start_time}, end_time={self.end_time}, duration={self.duration}, speaker={self.speaker}, text={self.text})"
 
 
-def parse_srt_to_arr(subtitles_str: str):
+MIN_GAP_BETWEEN_SUBS_IN_SECS = timedelta(seconds=0.5)
+MAX_SUB_TEXT_LENGTH_IN_SYMBOLS = 250
+
+
+def correct_subtitles_length(subs_arr) -> str:
+    new_subs_arr = []
+    index = 0
+    i = 0
+    while i < len(subs_arr):
+        current_sub = subs_arr[i]
+        if i + 1 < len(subs_arr):
+            next_sub = subs_arr[i + 1]
+            gap = next_sub.start_time - current_sub.end_time
+            tex_len = len(current_sub.text) + len(next_sub.text)
+            if gap <= MIN_GAP_BETWEEN_SUBS_IN_SECS and tex_len <= MAX_SUB_TEXT_LENGTH_IN_SYMBOLS:
+                # Concat subtitles
+                new_start_time = current_sub.start_time
+                new_end_time = next_sub.end_time
+                new_text = current_sub.text + ' ' + next_sub.text
+                index += 1
+                new_subtitle = Subtitle(
+                    id=index, 
+                    start_time=new_start_time, 
+                    end_time=new_end_time, 
+                    text=new_text
+                    )
+                new_subs_arr.append(new_subtitle)
+                i += 2
+                continue
+
+        index += 1
+        new_subtitle = Subtitle(
+            id=index, 
+            start_time=current_sub.start_time, 
+            end_time=current_sub.end_time,
+            text=current_sub.text
+            )
+        new_subs_arr.append(new_subtitle)
+        i += 1
+
+    new_subs_arr = fix_subtitles_sentenses(new_subs_arr)
+
+    srt_output = convert_subs_arr_to_srt(new_subs_arr)
+    return srt_output
+
+
+def fix_subtitles_sentenses(subs_arr):
+    for i in range(1, len(subs_arr)-1):
+        current_text = subs_arr[i].text
+        previous_text = subs_arr[i-1].text
+
+        # Checking if there are sentence fragments in the current subtitle
+        if re.search(r'[.!?]', current_text):
+            sentences = re.split(r'([.!?])', current_text)
+            # Concat sentences and punctuation
+            last_sentence = sentences[-1]
+            sentences = [sentences[i] + sentences[i+1] for i in range(0, len(sentences)-1, 2)]
+            if len(last_sentence) > 1:
+                sentences.append(last_sentence)
+
+            if len(sentences) > 1 and len(sentences[0].split()) < 4 and not re.search(r'[.!?]$', previous_text):
+                # Move the first sentence to the previous subtitle
+                subs_arr[i-1].text = previous_text + ' ' + sentences[0]
+                subs_arr[i].text = ' '.join(sentences[1:])
+    return subs_arr
+
+def convert_subs_arr_to_srt(subs_arr: list):
+    srt_output = ""
+    for sub in subs_arr:
+        srt_output += f"{sub.id}\n"
+        srt_output += f"{format_time_ms_to_str(sub.start_time)} --> {format_time_ms_to_str(sub.end_time)}\n"
+        srt_output += f"{sub.text}\n\n"
+    return srt_output
+    
+
+def write_subs_arr_to_srt_file(subs_arr: list, output_file_path: str):
+    srt_string_out = convert_subs_arr_to_srt(subs_arr)
+    with open(output_file_path, "w", encoding="utf-8") as f_out:
+        f_out.write(srt_string_out)
+
+
+def write_subs_arr_to_json_file(subtitles, filename):
+    subtitles_dict = [subtitle_to_dict(subtitle) for subtitle in subtitles]
+    with open(filename, 'w', encoding='utf-8') as file:
+        json.dump(subtitles_dict, file, ensure_ascii=False, indent=4)
+
+
+def parse_json_to_subtitles(json_filename: str):
+    with open(json_filename, 'r', encoding='utf-8') as file:
+        subtitles_data = json.load(file)
+
     subtitles = []
-    lines = subtitles_str.splitlines()
+    for data in subtitles_data:
+        start_time = parse_time_str_to_ms(data['start'])
+        end_time = parse_time_str_to_ms(data['end'])
+        subtitle = Subtitle(
+            id=data['id'],
+            start_time=start_time,
+            end_time=end_time,
+            text=data['text'],
+            speaker=data['speaker']
+        )
+        subtitles.append(subtitle)
+
+    return subtitles
+
+
+def subtitle_to_dict(subtitle: Subtitle):
+    return {
+        "id": subtitle.id,
+        "start": format_time_ms_to_str(subtitle.start_time),
+        "end": format_time_ms_to_str(subtitle.end_time),
+        "text": subtitle.text,
+        "speaker": subtitle.speaker
+    }
+
+
+def format_time_ms_to_str(time_ms: int):
+    milliseconds = time_ms % 1000
+    seconds = (time_ms // 1000) % 60
+    minutes = (time_ms // (1000 * 60)) % 60
+    hours = time_ms // (1000 * 60 * 60)
+    return f"{hours:02}:{minutes:02}:{seconds:02},{milliseconds:03}"
+
+
+def parse_time_str_to_ms(time_str:str):
+    """Takes a time string in the format "HH:MM:SS.mmm" and converts it to a total number of milliseconds."""
+    hours, minutes, seconds = map(float, time_str.split(':'))
+    milliseconds = int((seconds % 1) * 1000)
+    seconds = int(seconds)
+    total_ms = int(hours * 3600000 + minutes * 60000 + seconds * 1000 + milliseconds)
+    return total_ms
+
+
+def parse_srt_to_arr_from_file(file_path: str):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        subtitles_str = file.read()
+
+    return parse_srt_to_arr(subtitles_str)
+
+
+def parse_srt_to_arr(subtitles_srt_string: str):
+    subtitles = []
+    lines = subtitles_srt_string.splitlines()
 
     i = 0
     while i < len(lines):
@@ -30,11 +173,9 @@ def parse_srt_to_arr(subtitles_str: str):
             i += 1
             continue
 
-        start_time_str, end_time_str = time_match.groups()
-        start_time = datetime.strptime(start_time_str, '%H:%M:%S,%f')
-        end_time = datetime.strptime(end_time_str, '%H:%M:%S,%f')
-        duration_td = end_time - start_time
-        duration = duration_td.total_seconds()
+        start_time_str, end_time_str = time_match.groups()  # Find groups in the regex match
+        start_time_ms = parse_time_str_to_ms(start_time_str.replace(',', '.'))
+        end_time_ms = parse_time_str_to_ms(end_time_str.replace(',', '.'))
 
         text = []
         i += 2
@@ -43,109 +184,13 @@ def parse_srt_to_arr(subtitles_str: str):
             i += 1
 
         text = '\n'.join(text)
-        subtitle = Subtitle(number, start_time, end_time, duration, text)
+        subtitle = Subtitle(
+            id=number, 
+            start_time=start_time_ms, 
+            end_time=end_time_ms,
+            text=text
+            )
         subtitles.append(subtitle)
         i += 1
 
     return subtitles
-
-
-def parse_srt_to_arr_from_file(file_path: str):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        subtitles_str = file.read()
-
-    return parse_srt_to_arr(subtitles_str)
-
-
-MIN_GAP_BETWEEN_SUBS_IN_SECS = timedelta(seconds=0.5)
-MAX_SUB_TEXT_LENGTH_IN_SYMBOLS = 250
-
-
-def correct_subtitles_length(subtitles: str) -> str:
-    subs_arr = parse_srt_to_arr(subtitles)
-    new_subs_arr = []
-    index = 0
-
-    i = 0
-    while i < len(subs_arr):
-        current_sub = subs_arr[i]
-        if i + 1 < len(subs_arr):
-            next_sub = subs_arr[i + 1]
-            gap = next_sub.start_time - current_sub.end_time
-            tex_len = len(current_sub.text) + len(next_sub.text)
-            if gap <= MIN_GAP_BETWEEN_SUBS_IN_SECS and tex_len <= MAX_SUB_TEXT_LENGTH_IN_SYMBOLS:
-                # Concat subtitles
-                new_start_time = current_sub.start_time
-                new_end_time = next_sub.end_time
-                new_duration_td = new_end_time - new_start_time
-                new_duration = new_duration_td.total_seconds()
-
-
-
-                new_text = current_sub.text + ' ' + next_sub.text
-                index += 1
-                new_subtitle = Subtitle(index, new_start_time, new_end_time, new_duration, new_text)
-                new_subs_arr.append(new_subtitle)
-                i += 2
-                continue
-
-        index += 1
-        new_subtitle = Subtitle(index, current_sub.start_time, current_sub.end_time, current_sub.duration,
-                                current_sub.text)
-        new_subs_arr.append(new_subtitle)
-        i += 1
-
-    new_subs_arr = fix_subtitles_sentenses(new_subs_arr)
-
-    # Преобразуем новый массив субтитров обратно в строку формата SRT
-    srt_output = convert_subs_arr_to_str(new_subs_arr)
-
-    return srt_output
-
-
-
-def fix_subtitles_sentenses(subtitles):
-    for i in range(1, len(subtitles)-1):
-        current_text = subtitles[i].text
-        previous_text = subtitles[i-1].text
-
-        # Checking if there are sentence fragments in the current subtitle
-        if re.search(r'[.!?]', current_text):
-            sentences = re.split(r'([.!?])', current_text)
-            # Concat sentences and punctuation
-            last_sentence = sentences[-1]
-            sentences = [sentences[i] + sentences[i+1] for i in range(0, len(sentences)-1, 2)]
-            if len(last_sentence) > 1:
-                sentences.append(last_sentence)
-
-            if len(sentences) > 1 and len(sentences[0].split()) < 4 and not re.search(r'[.!?]$', previous_text):
-                # Move the first sentence to the previous subtitle
-                subtitles[i-1].text = previous_text + ' ' + sentences[0]
-                subtitles[i].text = ' '.join(sentences[1:])
-
-        # # Checking if there are sentence fragments in the previous subtitle
-        # if re.search(r'[.!?]', previous_text):
-        #     sentences = re.split(r'([.!?])', previous_text)
-        #     # Concat sentences and punctuation
-        #     sentences = [sentences[i] + sentences[i+1] for i in range(0, len(sentences)-1, 2)]
-
-        #     if len(sentences) > 1 and len(sentences[-1].split()) < 3:
-        #         # Move the last sentence to the current subtitle
-        #         subtitles[i-1].text = ' '.join(sentences[:-1])
-        #         subtitles[i].text = sentences[-1] + ' ' + current_text
-
-    return subtitles
-
-def convert_subs_arr_to_str(subs_arr: list):
-    srt_output = ""
-    for sub in subs_arr:
-        srt_output += f"{sub.number}\n"
-        srt_output += f"{sub.start_time.strftime('%H:%M:%S,%f')[:-3]} --> {sub.end_time.strftime('%H:%M:%S,%f')[:-3]}\n"
-        srt_output += f"{sub.text}\n\n"
-    return srt_output
-    
-
-def write_subs_arr_to_srt_file(subs_arr: list, output_file_path: str):
-    str_output = convert_subs_arr_to_str(subs_arr)
-    with open(output_file_path, "w", encoding="utf-8") as f_out:
-        f_out.write(str_output)
--- nodes/subtitles_generator_node.py
+++ nodes/subtitles_generator_node.py
@@ -1,42 +1,62 @@
-from pathlib import Path
+import json
+import os
 
 from config import AAI_API_KEY
 import assemblyai as aai
 
-from external_modules import sub_parser
 from external_modules.audio_extractor import extract_audio_from_video
+from external_modules.sub_parser import format_time_ms_to_str
 
 class VoceToSubtitlesNode:
     """Module for generating subtitles on original language"""
 
-    def __init__(self, src_lang: str) -> None:
+    def __init__(self, src_lang: str, num_of_speakers: int = None) -> None:
         aai.settings.api_key = AAI_API_KEY
-        self.aai_conf = aai.TranscriptionConfig(language_code=src_lang, punctuate=True)
+        self.src_lang = src_lang
+        self.aai_conf = aai.TranscriptionConfig(
+            language_code=src_lang, 
+            punctuate=True, 
+            speaker_labels=True,
+            speakers_expected=num_of_speakers,
+            speech_model=aai.SpeechModel.nano
+            )
 
-    def transcript(self, video_file_path: str, out_file_path: str):
-        video_file_name = video_file_path.split("\\")[-1].split("/")[-1]
-        temp_path = video_file_name.split(".")[-2]
-        if len(temp_path) < 2:
-            temp_path = video_file_name
+    def transcript(self, video_file_path: str, output_dir: str | None = None):
+        video_dir, video_filename = os.path.split(video_file_path)
+        if output_dir is None:
+            output_dir = video_dir
+        elif not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+            
+        
+        
+        out_filename = video_filename.split(".")[-2]
 
-        out_folder_path = video_file_path.replace(video_file_name, "")
-        if len(out_folder_path) != 0:
-            audio_file_path = f"{out_folder_path}\\audio_{temp_path}.wav"
-        else:
-            audio_file_path = f"audio_{temp_path}.wav"
+        audio_file_path = os.path.join(output_dir, f"audio_{out_filename}_{self.src_lang}.wav")
 
         extract_audio_from_video(video_file_path, audio_file_path)
         
-        transcript = aai.Transcriber().transcribe(video_file_path, self.aai_conf)
+        transcript = aai.Transcriber().transcribe(audio_file_path, self.aai_conf)
 
-        # Ensure the output directory exists
-        output_dir = Path(out_file_path).parent
-        output_dir.mkdir(parents=True, exist_ok=True)
-
+        # Export subtitles as srt
         subtitles = transcript.export_subtitles_srt()
-
-        subtitles = sub_parser.correct_subtitles_length(subtitles)
-
-        with open(out_file_path, "w", encoding="utf-8") as srt_file:
+        srt_out_filepath = os.path.join(output_dir,  f"{out_filename}_{self.src_lang}.srt")
+        # subtitles = correct_subtitles_length(subtitles)
+        with open(srt_out_filepath, "w", encoding="utf-8") as srt_file:
             srt_file.write(subtitles)
+
+        #Export subtitles as json
+        json_out_filepath = os.path.join(output_dir,  f"{out_filename}_{self.src_lang}.json")
+        subs_json_arr = []
+        for index, utterance in enumerate(transcript.utterances):
+            new_sub = {
+                "id": index,
+                "speaker": utterance.speaker,
+                "text": utterance.text,
+                "start": format_time_ms_to_str(utterance.start),
+                "end": format_time_ms_to_str(utterance.end)
+            }
+            subs_json_arr.append(new_sub)
+        with open(json_out_filepath, 'w', encoding='utf-8') as file:
+            json.dump(subs_json_arr, file, ensure_ascii=False, indent=4)
 
