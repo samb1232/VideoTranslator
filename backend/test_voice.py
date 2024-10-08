011ba49225d576938e2f74eabc4f8310469706cf
samb1232 <arterm212@gmail.com>
2024-10-08 16:18:51 +0500

    deleted unnecessary files

--- backend/test_voice.py
+++
@@ -1,9 +0,0 @@
-from modules.voice_generator import VoiceGenerator
-
-
-sg = VoiceGenerator("en", "materials\\2_audio.wav")
-
-text = "Swear test. Fuck me, this shit is fucking awersome. A fucking hate this cunt!"
-sg._synthesize(text_to_speak=text, out_wav_filepath="test_swears.wav")
-
-print("Done!")
