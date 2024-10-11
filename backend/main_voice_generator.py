c7d6e91563c4333fb8633cc18a19b1527c43ef4a
samb1232 <arterm212@gmail.com>
2024-10-12 01:10:51 +0500

    updated mains

--- backend/main_voice_generator.py
+++ backend/main_voice_generator.py
@@ -4,8 +4,8 @@ from modules.voice_generator import VoiceGenerator
 sg_node = VoiceGenerator(language="ru")
 
 sg_node.generate_audio(
-    orig_wav_filepath="test_files\\0000\\test_en.wav",
-    json_subs_filepath="test_files\\0000\\test_ru.json",
-    out_wav_filepath="test_files\\0000\\test_ru.wav"
+    orig_wav_filepath="backend\\test_files\\0004\\audio_0004.wav",
+    json_subs_filepath="backend\\test_files\\0004\\0004_ru.json",
+    out_wav_filepath="backend\\test_files\\0004\\FINAL.wav"
 )
 print("Done!")
