18417c5e0e7a40ae80d88cbc8c8847888bad79a4
samb1232 <arterm212@gmail.com>
2025-01-28 20:06:16 +0500

    fix/refactor app.py

--- backend/flask_api/app.py
+++ backend/flask_api/app.py
@@ -1,26 +1,25 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_session import Session
-from logging_conf import setup_logging
 from config import ConfigWeb
 from database.db_helper import DbHelper
 from routes import bp
 
 
-logger = setup_logging()
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(ConfigWeb)
+    app.register_blueprint(bp)
+    CORS(app, supports_credentials=True)
+    server_session = Session(app)
 
-db_operations = DbHelper()
+    db_operations = DbHelper()
+    db_operations.add_users_from_json("users.json")
 
-app = Flask(__name__)
+    return app
 
-app.config.from_object(ConfigWeb)
-app.register_blueprint(bp)
 
-CORS(app, supports_credentials=True)
-
-server_session = Session(app)
-
-db_operations.add_users_from_json("users.json")
+app = create_app()
 
 if __name__ == '__main__':
     app.run(port=5000, host="0.0.0.0", debug=False)
